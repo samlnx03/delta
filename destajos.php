912e5159e0e31d0455efda3a073ac3a076cfaa76
samuel perez <samlnx03@hotmail.com>
2019-03-15 16:47:02 -0600

    se agrego proceso a los reportes de destajos y se incluyo corte a largo

--- destajos.php
+++ destajos.php
@@ -20,29 +20,43 @@ if (isset($_POST["recalc"])){
 	$q="ALTER TABLE destajosMDim AUTO_INCREMENT = 1";
 	$db->query($q);
 	// aserrio operadores
-	$q="INSERT into destajosMDim (fecha,empleado,nombre,pctj,idmov,idrepo,actividad,activ,cantidad,idtabla,especie,medidas,volpt,costo,destajo) 
-	select fecha, operador as empleado, e.nombre, pctjOp as pctj, m.id, m.idrepo, m.actividad, a.descrip as activ, m.cantidad, m.idtabla, t.especie, t.descrip as medidas,  t.volpt*cantidad as volpt, a.costo, round(a.costo*volpt*cantidad*pctjOp/100,2) as destajo
+	$q="INSERT into destajosMDim (fecha,empleado,nombre,pctj,idmov,idrepo,actividad,activ,cantidad,idtabla,especie,medidas,volpt,costo,destajo,proceso) 
+	select fecha, operador as empleado, e.nombre, pctjOp as pctj, m.id, m.idrepo, m.actividad, a.descrip as activ, m.cantidad, m.idtabla, t.especie, t.descrip as medidas,  t.volpt*cantidad as volpt, a.costo, round(a.costo*volpt*cantidad*pctjOp/100,2) as destajo, a.proceso
 	from movsRepoDimensionado m
 	     left join repoProd rp1 on m.idRepo=rp1.id
 		left join tablas t on m.idtabla=t.id
 		left join actividades a on m.actividad=a.clave
 		left join empleados e on operador=e.id
-	where e.id IS NOT NULL AND $rangoFechas
+	where e.id IS NOT NULL AND a.proceso=1 AND $rangoFechas
 ";
 	//echo $q;
 	$db->query($q);
 	// aserrio ayudantes
-	$q="INSERT into destajosMDim (fecha,empleado,nombre,pctj,idmov,idrepo,actividad,activ,cantidad,idtabla,especie,medidas,volpt,costo,destajo) 
-	select fecha, ayudante as empleado, e.nombre, pctjAyu as pctj, m.id, m.idrepo, m.actividad, a.descrip as activ, m.cantidad, m.idtabla, t.especie, t.descrip as medidas,  t.volpt*cantidad as volpt, a.costo, round(a.costo*volpt*cantidad*pctjOp/100,2) as destajo
+	$q="INSERT into destajosMDim (fecha,empleado,nombre,pctj,idmov,idrepo,actividad,activ,cantidad,idtabla,especie,medidas,volpt,costo,destajo,proceso) 
+	select fecha, ayudante as empleado, e.nombre, pctjAyu as pctj, m.id, m.idrepo, m.actividad, a.descrip as activ, m.cantidad, m.idtabla, t.especie, t.descrip as medidas,  t.volpt*cantidad as volpt, a.costo, round(a.costo*volpt*cantidad*pctjAyu/100,2) as destajo, a.proceso
 	from movsRepoDimensionado m
 	     left join repoProd rp1 on m.idRepo=rp1.id
 		left join tablas t on m.idtabla=t.id
 		left join actividades a on m.actividad=a.clave
 		left join empleados e on ayudante=e.id
-	where e.id IS NOT NULL AND $rangoFechas
+	where e.id IS NOT NULL AND a.proceso=1 AND $rangoFechas
 ";
 	//echo $q;
 	$db->query($q);
+
+	//corte a largo
+	$q="INSERT into destajosMDim (fecha,empleado,nombre,pctj,idmov,idrepo,actividad,activ,cantidad,idtabla,especie,medidas,volpt,costo,destajo,proceso) 
+	select fecha, operador as empleado, e.nombre, 100 as pctj, m.id, m.idrepoCL, m.actividad, a.descrip as activ, m.cantidad, m.idtabla, t.especie, t.descrip as medidas,  t.volpt*cantidad as volpt, a.costo, round(a.costo*volpt*cantidad/100,2) as destajo, a.proceso
+	from movsRepoCL m
+	     left join repoCL rp1 on m.idRepoCL=rp1.id
+		left join tablas t on m.idtabla=t.id
+		left join actividades a on m.actividad=a.clave
+		left join empleados e on operador=e.id
+		where e.id IS NOT NULL AND a.proceso=2 AND $rangoFechas
+";
+	echo $q;
+	$db->query($q);
+	
 }
 $_SESSION["msg"]="Periodo de destajos actualizado";
 header("Location: reportes.php");
--- rdestajosAMD.php
+++ rdestajosAMD.php
@@ -24,19 +24,31 @@ require_once "funcs.php";  // funciones utiles
 require('menu.php');
 //require("menuProd.php");
 ?>
-<h1>Reporte de destajos de Aserrío</h1>
 <?php
 $db=db::getInstance();
+
+$proceso=NULL;
+if(isset($_POST['destajos_proceso'])) 
+	$proceso=$_POST['destajos_proceso'];
+
+if($proceso==1)
+	$trepo="<h1>Reporte de destajos de Aserrío</h1>\n";
+elseif($proceso==2)
+	$trepo="<h1>Reporte de destajos de Corte a Largo</h1>\n";
+else
+	$trepo="<h1>Ups, esto no debia pasar!</h1>\n";
 
 if(isset($_POST['todosRepos'])){
 	$f1=$_POST['f1'];
 	$f2=$_POST['f2'];
+	echo "$trepo\n";
 	echo "Periodo del $f1 al $f2<br>\n";
-	$q="select any_value(nombre) as nombre, sum(volpt) as volumen, sum(destajo) as destajo from destajosMDim group by nombre";
+	$q="select any_value(nombre) as nombre, sum(volpt) as volumen, sum(destajo) as destajo from destajosMDim where proceso='$proceso' group by nombre";
 }
 elseif(isset($_POST['xrepo'])){
 	$nrepo=htmlNpost('nrepo');
-	$q="select * from destajosMDim where idRepo=$nrepo";
+	$q="select * from destajosMDim where proceso='$proceso' AND idRepo=$nrepo";
+	echo "$trepo\n";
 	echo "Reporte $nrepo<br>\n";
 	//$q="select any_value(nombre) as nombre, sum(volpt) as volumen, sum(destajo) as destajo from destajosMDim where idRepo=$nrepo group by nombre";
 }
@@ -44,13 +56,14 @@ elseif(isset($_POST['xempleado'])){
 	$persona=htmlNpost('empleado');
 	$f1=$_POST['f1'];
 	$f2=$_POST['f2'];
-	$q="select * from destajosMDim where empleado=$persona";
+	$q="select * from destajosMDim where proceso='$proceso' AND empleado=$persona";
+	echo "$trepo\n";
 	echo "Reporte del trabajador $persona, del $f1 al $f2<br>\n";
 }
 
 //$q="select any_value(r.sierraCinta) as Sierra, any_value(t.especie), sum(m.cantidad*t.volpt) as vol FROM repoProd r LEFT JOIN movsRepoDimensionado as m ON r.id=m.idRepo LEFT JOIN actividades as a ON m.actividad=a.clave LEFT JOIN tablas as t ON m.idtabla=t.id WHERE fecha='2018-5-31' and r.sierraCinta=1 and a.proceso=1 group by especie ";
 //$q="select r.fecha, r.id as Repo, r.sierraCinta as Sierra, m.cantidad, t.especie, t.descrip as dimensiones, m.cantidad*t.volpt as vol FROM repoProd r LEFT JOIN movsRepoDimensionado as m ON r.id=m.idRepo LEFT JOIN actividades as a ON m.actividad=a.clave LEFT JOIN tablas as t ON m.idtabla=t.id WHERE fecha='2018-5-31' and a.proceso=1 order by sierra,especie,grueso,ancho,largo";
-echo "$q<br>\n";
+//echo "$q<br>\n";
 $db->query($q);
 $t=new html_table();
 //$t->setcdatas(array("cant" => "cantidad", "descrip"=>"descrip", "especie"=>"especie", "dimensiones"=>"dimensiones" ));
